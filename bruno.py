4352ad25205fd9acf789aacceb60fccc11d395ea
zell-tec <brunobrandao5577@gmail.com>
2024-09-13 21:51:21 -0300

    Add files via upload

---
+++ bruno.py
@@ -0,0 +1,8 @@
+# Autor: Bruno Nunes
+# Data: agosto/2024
+# Codigos iniciais Python
+
+idade = eval(input ("informe sua idade"))
+if idade<5:
+    print('a criança deve ser vacinada contra gripe.')
+print('cuide da saúde sempre. Até a proxima')
